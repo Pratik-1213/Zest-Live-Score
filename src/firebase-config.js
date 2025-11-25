919ac8a0dc5958046aaa49149edae8f73039bc76
Purva Nitin Rane <purvarane.2623@gmail.com>
2025-11-26 02:41:09 +0530

    Code Added to Repo

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+// For simplicity in this example, we'll use a placeholder configuration.
+// In a real-world application, these keys would be stored securely.
+const firebaseConfig = {
+  apiKey: "AIzaSyDfVEF4OnbYEXZCkjg0957WDMQF_Ms0zeg",
+  authDomain: "sports-live-hub.firebaseapp.com",
+  projectId: "sports-live-hub",
+  storageBucket: "sports-live-hub.firebasestorage.app",
+  messagingSenderId: "898208210644",
+  appId: "1:898208210644:web:8188f740a07f021dbb6541",
+  measurementId: "G-CGY8EVPBWB"
+};
+
+export default firebaseConfig;
